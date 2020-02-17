2a969b9c517c9b87eedfc0eefca436ab8f586631
BigKupa <avkuprianov@mail.ru>
2020-02-17 16:59:51 +0300

    Added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $(function(){
-
-
-
-
+	var mixer = mixitup('.products__inner-box');
+	$(".rate__star").rateYo({
+		rating: 3.5,
+		starWidth: "12px",
+		readOnly: true
+	});
+	$('.product__slider-inner').slick({
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		arrows: false
+	})
 });
