6c8561b55c39619f99dcc90910f8d728bee7c5b0
BigKupa <avkuprianov@mail.ru>
2020-02-23 14:07:00 +0300

    New items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-	var mixer = mixitup('.products__inner-box');
 	$(".rate__star").rateYo({
 		rating: 3.5,
 		starWidth: "12px",
@@ -10,5 +9,24 @@ $(function(){
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		arrows: false
-	})
+	});
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+	$('.icon-th-list').on('click', function(){
+		$('.product__item').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.icon-th-large').removeClass('active');
+	});
+	$('.icon-th-large').on('click', function(){
+		$('.product__item').removeClass('list');
+		$('.icon-th-list').removeClass('active');
+		$('.icon-th-large').addClass('active');
+	});
+	var mixer = mixitup('.products__inner-box');
 });
